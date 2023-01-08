2cb9d7aa9efd4a908a87b64616fa73142e1bafd6
LeeZard1205 <eternitydarkfire@gmail.com>
2023-01-08 09:38:35 +0700

    weed Commit

--- JSA16/Day 6/RegisAndLogin/login.js
+++ JSA16/Day 6/RegisAndLogin/login.js
@@ -1,30 +1,34 @@
 let mail = document.getElementById("email");
 let mk = document.getElementById("mk");
-let check = 0;
-
-if (localStorage.getItem("personAccount") == null) {
-  check = 1;
-} else {
-  var listUser = JSON.parse(localStorage.getItem("personAccount"));
-}
 
 function checkinf() {
-  if (mail.value == "" || mk.value == "") {
-    alert("Please fill in the information below!");
-  } else if (check == 1) {
-    alert("Please check your email and password and try again!");
+  if (
+    localStorage.getItem("personAccount") == null ||
+    localStorage.getItem("personAccount") == JSON.stringify([])
+  ) {
+    alert("You haven't got an account. Please register!");
+    window.location.href = "register.html";
   } else {
-    for (let i = 0; i < listUser.length; i++) {
-      if (listUser[i].email == mail.value && listUser[i].password == mk.value) {
-        alert("Logged in successfully");
-        mail.value = "";
-        mk.value = "";
-        check = 1;
-        break;
+    var listUser = JSON.parse(localStorage.getItem("personAccount"));
+    if (mail.value == "" || mk.value == "") {
+      alert("Please fill in the information below!");
+    } else {
+      let check = 0;
+      for (let i = 0; i < listUser.length; i++) {
+        if (
+          listUser[i].email == mail.value &&
+          listUser[i].password == mk.value
+        ) {
+          alert("Logged in successfully");
+          mail.value = "";
+          mk.value = "";
+          check = 1;
+          break;
+        }
       }
-    }
-    if (check == 0) {
-      alert("Please check your email and password and try again!");
+      if (check == 0) {
+        alert("Please check your email and password and try again!");
+      }
     }
   }
 }
